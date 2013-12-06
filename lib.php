4c9736dff18cf629592ab08a84be9ff5c316aee5
tomleger <tleger@gmail.com>
2013-12-05 19:54:46 -0500

    Create lib.php

---
+++ lib.php
@@ -0,0 +1,23 @@
+<?php
+
+//Create an adgroup object - make HTTP POST call to:
+
+ //"https://graph.facebook.com/act_{ad_account_id}/adgroups"
+
+$acct_id = " "; //int
+
+
+$call = "https://graph.facebook.com/act_" . $acct_id "/adgroups";
+
+$name = " "; //string - required
+$adgroup_specs = " "; //array - required
+$notification_url = " ";
+$notification_mode = " ";
+
+
+
+
+
+
+
+?>
